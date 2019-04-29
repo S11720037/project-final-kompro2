b54de50946f7c405c67627631c0344572f6803d8
S11720037 <50015208+S11720037@users.noreply.github.com>
2019-04-29 18:28:25 +0800

    Nilai final dan mid semester telah ditambahkan

--- main.cpp
+++ main.cpp
@@ -17,6 +17,27 @@ struct nilai_kuis{
 	
 };
 
+//struktur untuk nilai projek
+struct nilai_project{
+	
+	int nilai;
+	
+};
+
+//struktur untuk nilai mid semester
+struct nilai_mid{
+	
+	int nilai;
+	
+};
+
+//struktur untuk nilai final
+struct nilai_final{
+	
+	int nilai;
+	
+};
+
 //struktur untuk menyimpan data mahasiswa
 struct siswa{
 	
@@ -25,6 +46,9 @@ struct siswa{
 	
 	struct nilai_tugas tugas[3];
 	struct nilai_kuis kuis[4];
+	struct nilai_project project;
+	struct nilai_mid mid;
+	struct nilai_final final;
 };
 
 int main(){
@@ -67,11 +91,27 @@ int main(){
 		scanf("%d",&mahasiswa[i].kuis[j].nilai);
 		}
 		
+		//masukan nilai project
+		printf("\nProject\n");
+		printf("Masukan nilai : ");
+		scanf("%d",&mahasiswa[i].project.nilai);
+		
+		
+		//memasukan nilai mid
+		printf("\nMID\n");
+		printf("Masukan nilai : ");
+		scanf("%d",&mahasiswa[i].mid.nilai);
+		
+		//memasukan nilai final
+		printf("\nFinal\n");
+		printf("Masukan nilai : ");
+		scanf("%d",&mahasiswa[i].final.nilai);
+		
 	}
 	
 	//menampilkan data yang telah dimasukan
-	printf("NIM\tNama\tTugas 20a\t\tKuis 20a\n");
-	printf("\t\tI\tII\tIII\t20a\tI\tII\tIII\tIV\t15a\n");
+	printf("NIM\tNama\tTugas 20a\t\t\tKuis 10a\t\t\tProject 15a\tMID 25a\tFinal 25a\n");
+	printf("\t\tI\tII\tIII\t20a\tI\tII\tIII\tIV\t10a\t15a\t25a\t25a\n");
 		
 	for(int i = 0; i < jumlah_data; i++){
 		
@@ -83,7 +123,7 @@ int main(){
 		
 		for(int j = 0; j < 3; j++){
 			
-			total_tugas += mahasiswa[i].tugas[j].nilai * 20 / 100;
+			total_tugas = mahasiswa[i].tugas[j].nilai * 20 / 100;
 			
 			printf("\t%d",mahasiswa[i].tugas[j].nilai);
 		}
@@ -93,12 +133,21 @@ int main(){
 		int total_kuis;
 		for(int j = 0; j < 4; j++){
 			
-			total_kuis += mahasiswa[i].kuis[j].nilai * 15 / 100;
+			total_kuis = mahasiswa[i].kuis[j].nilai * 10 / 100;
 			
 			printf("\t%d",mahasiswa[i].kuis[j].nilai);
 		}
 		printf("\t%d",total_kuis);
 		
+		//menampilkan nilai project
+		printf("\t%d",mahasiswa[i].project.nilai * 15 / 100);
+	
+	
+		//menampilkan nilai mid
+		printf("\t%d",mahasiswa[i].mid.nilai * 25 / 100);
+	
+		//menampilkan nilai final
+		printf("\t%d",mahasiswa[i].final.nilai * 25 / 100);
 	
 		printf("\n");
 		
